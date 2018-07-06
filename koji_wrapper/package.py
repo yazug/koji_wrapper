f7db61897ae4b373e1858f794601de3ba50c4033
Jon Schlueter <jschluet@redhat.com>
2018-07-06 15:33:28 -0400

    KojiPackage object hacking - initial prototype
    Local testing for idea of Wrapper object for KojiPackage wrapping koji api listPackage
    
    p1 = KojiPackage(session=wrapper, tagID=wrapper.session.getTag('some-tag'), inherited=True)
    
    for i in p1:
        print(i['package_name')
    
    Nice thing is you take the hit for the first time you get the iterator or call fetch_data()
    but after that the data is local and you can just iterate through it with no issues
    
    [i['package_id'] for i in p1]

--- koji_wrapper/package.py
+++ koji_wrapper/package.py
@@ -10,30 +10,44 @@ from koji_wrapper.util import convert_to_list
 class KojiPackage(KojiWrapper):
     """Class to work with and gather koji package information"""
 
-    def __init__(self, tag_id=None, userID=None, pkgID=None, **kwargs):
-        self._list_packages_args = {}
+    def __init__(self, **kwargs):
+        self.__list_packages_args = {}
         # TODO(jmls): figure out better way of seperating out kwargs
         self.list_packages_args = kwargs
+
+        for arg in ('pkgID', 'userID', 'tagID', 'prefix', 'inherited', 'with_dups', 'event'):
+            if arg in kwargs:
+                del kwargs[arg]
+
+        self.__data_list = None
 
         super().__init__(**kwargs)
 
     @property
     def list_packages_args(self):
-        """:param tag_id: tag_id to filter results with."""
-        return self.__tag_id
+        """:param list_packages_args: list_packages_args to filter results with."""
+        return self.__list_packages_args
 
-    @tag.setter
-    def list_packages_args(self, **kwargs):
-        for key, value in kwargs.items():
+    @list_packages_args.setter
+    def list_packages_args(self, args):
+        for key, value in args.items():
             if key in ('pkgID', 'userID', 'tagID', 'prefix', 'inherited', 'with_dups', 'event'):
                 if value is None and key in self.__list_packages_args:
                     del self.__list_packages_args[key]
                 else: 
                     self.__list_packages_args[key] = value
-            elif self.debug:
+            elif False:
                raise ValueError('Key {0} not on supported list for listPackages'.format(key))
 
+    def fetch_data(self):
+        self.__data_list = self.session.listPackages(**self.list_packages_args)
 
     def __iter__(self):
-        self.__list = self.session.listPackages(**self.list_packages_args)
-        return self.__list
+        if self.__data_list is None:
+            self.fetch_data()
+
+        self.__iterable = (obj for obj in self.__data_list)
+        return self.__iterable
+
+    def __next__(self):
+        return next(self.__iterable)
